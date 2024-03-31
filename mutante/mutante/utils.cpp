831ef268fe6a3f6e0022a3ccd09e6497e2ad8926
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-03-31 14:23:07 -0600

    Added validation

--- mutante/mutante/utils.cpp
+++ mutante/mutante/utils.cpp
@@ -96,17 +96,27 @@ PVOID Utils::FindPatternImage(PVOID base, const char* pattern, const char* mask)
 {
 	PVOID match = nullptr;
 
-	auto* headers = reinterpret_cast<PIMAGE_NT_HEADERS>(static_cast<char*>(base) + static_cast<PIMAGE_DOS_HEADER>(base)->e_lfanew);
-	auto* sections = IMAGE_FIRST_SECTION(headers);
-	
-	for (auto i = 0; i < headers->FileHeader.NumberOfSections; ++i) 
+	// Ensure base is valid
+	if (!base)
+		return nullptr;
+
+	// Calculate the address of the NT headers
+	auto* dosHeader = static_cast<PIMAGE_DOS_HEADER>(base);
+	auto* ntHeaders = reinterpret_cast<PIMAGE_NT_HEADERS>(reinterpret_cast<ULONG_PTR>(base) + dosHeader->e_lfanew);
+	if (dosHeader->e_magic != IMAGE_DOS_SIGNATURE || ntHeaders->Signature != IMAGE_NT_SIGNATURE)
+		return nullptr;
+
+	// Get the pointer to the first section
+	auto* section = IMAGE_FIRST_SECTION(ntHeaders);
+	for (auto i = 0; i < ntHeaders->FileHeader.NumberOfSections; ++i, ++section)
 	{
-		auto* section = &sections[i];
-		if ('EGAP' == *reinterpret_cast<PINT>(section->Name) || memcmp(section->Name, ".text", 5) == 0) 
+		// Check if the section is executable
+		if ((section->Characteristics & IMAGE_SCN_MEM_EXECUTE) != 0)
 		{
+			// Find the pattern in this section
 			match = FindPattern(static_cast<char*>(base) + section->VirtualAddress, section->Misc.VirtualSize, pattern, mask);
-			if (match) 
-				break;
+			if (match)
+				break; // Pattern found, exit the loop
 		}
 	}
 
