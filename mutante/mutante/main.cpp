0bbb1e10b5acca8fb04c5388ee17e31e70283e16
cvs0 <143862758+cvs0@users.noreply.github.com>
2024-03-31 14:24:49 -0600

    Refactor DriverEntry function for improved readability and add descriptive comments

--- mutante/mutante/main.cpp
+++ mutante/mutante/main.cpp
@@ -7,26 +7,27 @@
 #include <ntifs.h>
 #include "log.h"
 #include "shared.h"
-
 #include "disks.h"
 #include "smbios.h"
 
-/**
- * \brief Driver's main entry point
- * \param object Pointer to driver object (invalid when manual mapping)
- * \param registry Registry path (invalid when manual mapping)
- * \return Status of the driver execution
- */
+ /**
+  * \brief Driver's main entry point
+  * \param object Pointer to driver object (invalid when manually mapped)
+  * \param registry Registry path (invalid when manually mapped)
+  * \return Status of the driver execution
+  */
 extern "C" NTSTATUS DriverEntry(PDRIVER_OBJECT object, PUNICODE_STRING registry)
 {
-	UNREFERENCED_PARAMETER(object);
-	UNREFERENCED_PARAMETER(registry);
+    UNREFERENCED_PARAMETER(object);
+    UNREFERENCED_PARAMETER(registry);
 
-	Log::Print("Driver loaded. Build on %s.", __DATE__);
+    // Log driver load information
+    Log::Print("Driver loaded. Build on %s.", __DATE__);
 
-	Disks::DisableSmart();
-	Disks::ChangeDiskSerials();
-	Smbios::ChangeSmbiosSerials();
+    // Perform driver initialization tasks
+    Disks::DisableSmart();
+    Disks::ChangeDiskSerials();
+    Smbios::ChangeSmbiosSerials();
 
-	return STATUS_SUCCESS;
+    return STATUS_SUCCESS;
 }
